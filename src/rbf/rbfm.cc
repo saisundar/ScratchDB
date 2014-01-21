4042efc5b50bc09fe79b75e7788bcfd0873e519c
kewlgandhi <kewlgandhi@gmail.com>
2014-01-21 11:19:35 -0800

    Record manager basic functionalities

--- src/rbf/rbfm.cc
+++ src/rbf/rbfm.cc
@@ -1,4 +1,4 @@
-
+#include<cstdlib>
 #include "rbfm.h"
 
 RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
@@ -20,19 +20,34 @@ RecordBasedFileManager::~RecordBasedFileManager()
 }
 
 RC RecordBasedFileManager::createFile(const string &fileName) {
-    return -1;
+    PagedFileManager *pfm = PagedFileManager::instance();
+    pfm->createFile(fileName.c_str());
+    FileHandle fileHandle;
+    pfm->openFile(fileName.c_str(),fileHandle);
+//    void *data = calloc(4096,1);
+    void *data = malloc(4096);
+    *data = 0;
+    *(data+1) = 0;
+    fileHandle.appendPage(data);
+    return 0;
 }
 
 RC RecordBasedFileManager::destroyFile(const string &fileName) {
-    return -1;
+    PagedFileManager *pfm = PagedFileManager::instance();
+    pfm->destroyFile(fileName.c_str());
+    return 0;
 }
 
 RC RecordBasedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
-    return -1;
+    PagedFileManager *pfm = PagedFileManager::instance();
+    pfm->openFile(fileName.c_str(),fileHandle);
+    return 0;
 }
 
 RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+    PagedFileManager *pfm = PagedFileManager::instance();
+	pfm->closeFile(fileHandle);
+    return 0;
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
