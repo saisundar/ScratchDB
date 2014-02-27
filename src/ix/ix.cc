161cf0ca7a09d18282ab625e957a338517a0b233
kewlgandhi <kewlgandhi@gmail.com>
2014-02-27 08:29:07 -0800

    Minor implementations

--- src/ix/ix.cc
+++ src/ix/ix.cc
@@ -21,22 +21,30 @@ IndexManager::~IndexManager()
 
 RC IndexManager::createFile(const string &fileName)
 {
-	return -1;
+	PagedFileManager *pfm = PagedFileManager::instance();
+	if(pfm->createFile(fileName.c_str())==-1)return -1;
+	return 0;
 }
 
 RC IndexManager::destroyFile(const string &fileName)
 {
-	return -1;
+	PagedFileManager *pfm = PagedFileManager::instance();
+	if(pfm->destroyFile(fileName.c_str())==-1)return -1;
+	return 0;
 }
 
 RC IndexManager::openFile(const string &fileName, FileHandle &fileHandle)
 {
-	return -1;
+	PagedFileManager *pfm = PagedFileManager::instance();
+	if(pfm->openFile(fileName.c_str(), fileHandle)==-1)return -1;
+	return 0;
 }
 
 RC IndexManager::closeFile(FileHandle &fileHandle)
 {
-	return -1;
+	PagedFileManager *pfm = PagedFileManager::instance();
+	if(pfm->closeFile(fileHandle)==-1)return -1;
+	return 0;
 }
 
 RC IndexManager::insertEntry(FileHandle &fileHandle, const Attribute &attribute, const void *key, const RID &rid)
